7b4ff27734ac9dcede0b255a227b44cd07a05f3c
SimeonTsvetanov <SimeonTsvetanov@users.noreply.github.com>
2025-06-08 10:35:13 +0000

    deploy: e6a7780dae7874461cbee006a7ab9f74c1d0d87d

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Quizzard PWA
-// VERSION: 2025-06-07T16:04:37Z
+// VERSION: 2025-06-08T10:35:00Z
 // Handles caching, updates, and offline functionality
 // Version updated: June 6, 2025 - SPA routing fix
 
