fcf1bd77ca456070f78bd43460fe4f972bf29b39
SimeonTsvetanov <SimeonTsvetanov@users.noreply.github.com>
2025-06-09 20:50:43 +0000

    deploy: 5ab9c781bfdf1e3791a7110202d39f4598906a2e

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Quizzard PWA
-// VERSION: 2025-06-09T16:23:26Z
+// VERSION: 2025-06-09T20:50:27Z
 // Handles caching, updates, and offline functionality
 // Version updated: December 7, 2025 - Black bar fix and manifest update
 
