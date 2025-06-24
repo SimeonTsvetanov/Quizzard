a7e90f480ee1a154d76b9ae4343c4130fbddbe4c
SimeonTsvetanov <SimeonTsvetanov@users.noreply.github.com>
2025-06-24 17:33:36 +0000

    deploy: d92e6c3f0906ff12b0e66e4043c6ad742bc8f0ee

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for Quizzard PWA
-// VERSION: 2025-06-24T13:25:01Z
+// VERSION: 2025-06-24T17:33:24Z
 // Handles caching, updates, and offline functionality
 // Version updated: December 22, 2025 - Development and Production compatibility
 
